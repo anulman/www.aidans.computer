dc7c4dcf878cf2c833f2c723d69c330a6a11f71a
@anulman <anulman@gmail.com>
2017-12-25 14:43:26 -0700

    refactor(glitchy-canvas): read image from css + tidy js webgl implementation + update per shaders

--- app/pods/components/glitchy-canvas/component.js
+++ app/pods/components/glitchy-canvas/component.js
@@ -1,15 +1,18 @@
 import Ember from 'ember';
 
-import { cover } from '../../../utils/intrinsic-scale';
-import { randomNumber } from '../../../utils/random';
+import { createProgram } from 'ember-cli-webgl/utils';
+import vertexShader from '../../../shaders/vertex';
+import fragmentShader from '../../../shaders/fragment';
 
 const {
   Component,
   computed,
-  isBlank
+  isBlank,
+  isPresent
 } = Ember;
 
 const MAX_HORIZONTAL_OFFSET = 20;
+const CLOCK_START_MARK_NAME = 'ember-cli-webgl:clockStart';
 
 export default Component.extend({
   tagName: 'canvas',
@@ -17,89 +20,140 @@ export default Component.extend({
   imageUrl: null,
   maxHorizontalOffset: MAX_HORIZONTAL_OFFSET,
 
-  context: computed('element', function() {
+  gl: computed('element', function() {
     let canvas = this.get('element');
 
-    return isBlank(canvas) ? null : canvas.getContext('2d');
+    return isBlank(canvas) ? null : canvas.getContext('webgl');
+  }),
+
+  program: computed('gl', function() {
+    let gl = this.get('gl');
+
+    return createProgram(gl, vertexShader, fragmentShader);
   }),
 
   didInsertElement() {
-    this.image = new Image();
-    this.image.src = this.get('imageUrl');
-    this.image.onload = start.bind(this);
-  },
+    let { gl, program } = this.getProperties('gl', 'program');
 
-  draw(numRemaining) {
-    resetCanvasDimensions(this.element);
+    if (isPresent(program)) {
+      let image = new Image();
+      let element = this.element;
 
-    let { context, image } = this.getProperties('context', 'image');
-    let { innerWidth: windowWidth, innerHeight: windowHeight } = window;
-    let { width: imageWidth, height: imageHeight } = image;
-
-    let destination = cover(windowWidth, windowHeight, imageWidth, imageHeight);
-    let scale = image.height / destination.height;
-    let source = {
-      x: scale * destination.x * -1,
-      y: Math.min(scale * destination.y * -2, 100),
-      width: imageWidth,
-      height: imageHeight
-    };
-
-    destination.x = 0;
-    destination.y = 0;
-
-    if (isBlank(numRemaining) || numRemaining === 0) {
-      numRemaining = randomNumber(2, 5);
-
-      drawRaw(context, image, source, destination);
-      setTimeout(this.draw.bind(this, numRemaining), randomNumber(1500, 5000));
-    } else {
-      // todo: better glitch handling -- use shader?
-      let baseX = source.x;
-      let numVerticalSlices;
-      let offset = this.get('maxHorizontalOffset');
-
-      destination.height = randomNumber(20, 50);
-      source.height = destination.height * scale;
-
-      numVerticalSlices = Math.round(windowHeight / destination.height);
-
-      while (numVerticalSlices > 0) {
-        source.x = baseX + randomNumber(-Math.abs(offset), offset);
-
-        drawRaw(context, image, source, destination);
-
-        source.y += source.height;
-        destination.y += destination.height;
-
-        numVerticalSlices -= 1;
-      }
-
-      setTimeout(this.draw.bind(this, numRemaining - 1), randomNumber(30, 150));
+      image.src = this.getWithDefault('imageSrc', getBackgroundImage(element));
+      image.onload = startProgram.bind(this, image, gl, program);
     }
   }
 });
 
-function start() {
-  this.draw();
-  document.querySelector('body').style.background = 'unset';
+function getBackgroundImage(element) {
+  let string = getComputedStyle(element)['background-image'] || '';
+
+  return string
+    .replace('url("', '')
+    .replace('")', '');
 }
 
-function resetCanvasDimensions(canvas) {
-  canvas.width = window.innerWidth;
-  canvas.height = window.innerHeight;
-}
+function startProgram(image, gl, program) {
+  // n.b. in future iterations:
+  // - use GLSL AST to auto-extract variables
+  // - how can we identify multiple fragments?
+  let pointers = {
+    aImagePosition: {
+      location: gl.getAttribLocation(program, "a_image_position"),
+      buffer: gl.createBuffer()
+    },
+    aImageTexcoord: {
+      location: gl.getAttribLocation(program, "a_image_texcoord"),
+      buffer: gl.createBuffer()
+    },
+    iTime: {
+      location: gl.getUniformLocation(program, "iTime")
+    },
+    uScreenSize: {
+      location: gl.getUniformLocation(program, "u_screen_size")
+    },
+    uImageSize: {
+      location: gl.getUniformLocation(program, "u_image_size")
+    }
+  };
 
-function drawRaw(ctx, image, source, destination) {
-  ctx.drawImage(...[
+  gl.useProgram(program);
+  performance.mark(CLOCK_START_MARK_NAME);
+
+  loadTextureFor(gl, image, pointers);
+  resizeCanvas(gl, image, pointers);
+  drawScene(gl, pointers);
+
+  window.addEventListener('resize', resizeCanvas.bind(this, ...[
+    gl,
     image,
-    source.x,
-    source.y,
-    source.width,
-    source.height,
-    destination.x,
-    destination.y,
-    destination.width,
-    destination.height
-  ]);
+    pointers
+  ]));
+}
+
+function resizeCanvas(gl, image, { aImagePosition, aImageTexcoord }) {
+  gl.canvas.width = window.innerWidth;
+  gl.canvas.height = window.innerHeight;
+
+  bufferRectData(gl, aImagePosition, 0, 0, image.width, image.height);
+  bufferRectData(gl, aImageTexcoord);
+
+  gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+}
+
+function loadTextureFor(gl, image, pointers) {
+  let { uImageSize } = pointers;
+  let texture = gl.createTexture();
+
+  gl.bindTexture(gl.TEXTURE_2D, texture);
+
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+
+  gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
+  gl.uniform2f(uImageSize.location, image.width, image.height);
+
+  return texture;
+}
+
+function drawScene(gl, pointers) {
+  let [marker] = performance.getEntriesByName(CLOCK_START_MARK_NAME)
+  let { aImagePosition, aImageTexcoord, iTime, uScreenSize } = pointers;
+  let iTimeValue = (performance.now() - marker.startTime) / 1000;
+
+  gl.clearColor(0, 0, 0, 0);
+  gl.clear(gl.COLOR_BUFFER_BIT);
+
+  gl.enableVertexAttribArray(aImagePosition.location);
+  gl.bindBuffer(gl.ARRAY_BUFFER, aImagePosition.buffer);
+  gl.vertexAttribPointer(aImagePosition.location, 2, gl.FLOAT, false, 0, 0);
+
+  gl.enableVertexAttribArray(aImageTexcoord.location);
+  gl.bindBuffer(gl.ARRAY_BUFFER, aImageTexcoord.buffer);
+  gl.vertexAttribPointer(aImageTexcoord.location, 2, gl.FLOAT, false, 0, 0);
+
+  gl.uniform2f(uScreenSize.location, window.innerWidth, window.innerHeight);
+  gl.uniform1f(iTime.location, iTimeValue);
+
+  gl.drawArrays(gl.TRIANGLES, 0, 6);
+  requestAnimationFrame(drawScene.bind(this, gl, pointers, marker));
+}
+
+function bufferRectData(gl, { buffer }, x = 0, y = 0, width = 1, height = 1) {
+  let x1 = x;
+  let x2 = x + width;
+  let y1 = y;
+  let y2 = y + height;
+
+  gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([
+     x1, y1,
+     x2, y1,
+     x1, y2,
+     x1, y2,
+     x2, y1,
+     x2, y2
+  ]), gl.STATIC_DRAW);
 }
