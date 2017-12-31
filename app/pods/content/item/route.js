da0c2b36a37168d1c7938920ffe0124d7421672d
@anulman <anulman@gmail.com>
2017-12-31 15:31:43 -0500

    feat(content): add routes for hyde collections + items

---
+++ app/pods/content/item/route.js
@@ -0,0 +1,21 @@
+import Ember from 'ember';
+
+const { Route } = Ember;
+
+export default Route.extend({
+  model({ item_id }) {
+    return this.store.findRecord('hyde/item', `content/${item_id}`);
+  },
+
+  afterModel(model, transition) {
+    if (model.get('yaml') === undefined) {
+      let { item_id } = transition.params[this.routeName];
+
+      this.replaceWith('content.collection', item_id);
+    }
+  },
+
+  setupController(controller, model) {
+    controller.set('data', { item: model });
+  }
+});
