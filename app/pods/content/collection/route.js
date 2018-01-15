77338f929e5ffd3f17bf4adfface76b059cf9ced
@anulman <anulman@gmail.com>
2018-01-14 22:49:54 -0500

    feat(content): update routes for fastboot

--- app/pods/content/collection/route.js
+++ app/pods/content/collection/route.js
@@ -1,10 +1,17 @@
-import Ember from 'ember';
-
-const { Route } = Ember;
+import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
 
 export default Route.extend({
+  fastboot: service(),
+
   model({ collection_id }) {
     return this.store.findRecord('hyde/collection', `content/${collection_id}`);
+  },
+
+  afterModel(model) {
+    if (this.get('fastboot.isFastBoot')) {
+      return model.get('items');
+    }
   },
 
   setupController(controller, model) {
