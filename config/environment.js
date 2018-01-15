1ada1e7aa822422e4d5bb7ce1d8c4e039b720d8b
@anulman <anulman@gmail.com>
2018-01-14 21:53:57 -0500

    refactor(config): add fastboot hostWhitelist

--- config/environment.js
+++ config/environment.js
@@ -22,6 +22,10 @@ module.exports = function(environment) {
     APP: {
       // Here you can pass flags/options to your application instance
       // when it is created
+    },
+
+    fastboot: {
+      hostWhitelist: ['anulman.com', 'aidans.computer', /^localhost:\d+$/]
     }
   };
 
